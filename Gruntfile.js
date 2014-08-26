167dffc716d035aad910720ac46c9c206f57f14c
Ricardo Mestre <ricardo@duplos.org>
2014-08-26 10:22:23 +0100

    Watch assets subfolders

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,7 @@ module.exports = function(grunt) {
   grunt.initConfig({
     // https://github.com/gruntjs/grunt-contrib-clean
     clean : ['source/assets/styles/*', '!source/assets/**/.gitkeep', 'source/assets/scripts/*', '_site/*'],
-    
+
     // https://github.com/gruntjs/grunt-contrib-compass
     compass : {
       // Default options.
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
           cssDir : 'source/assets/styles',
           //raw : 'require "sassy-strings";'
       },
-      
+
       dev : {
         options : {
           environment: 'development',
@@ -26,24 +26,24 @@ module.exports = function(grunt) {
         }
       }
     },
-        
+
     // https://github.com/gruntjs/grunt-contrib-jshint
     jshint: {
       // Default options.
       options : {
         unused : true
       },
-      
+
       dev : {
         options : {
           force : true
         },
         src : ['source_assets/scripts/**.js']
       },
-      
+
       prod: ['source_assets/scripts/**.js']
     },
-    
+
     // https://github.com/gruntjs/grunt-contrib-uglify
     uglify: {
       prod: {
@@ -52,11 +52,11 @@ module.exports = function(grunt) {
         }
       }
     },
-    
+
     // https://npmjs.org/package/grunt-contrib-watch
     watch : {
       src: {
-        files: ['source_assets/scripts/**.js', 'source_assets/styles/**.scss'],
+        files: ['source_assets/scripts/**.js', 'source_assets/styles/**/*.scss'],
         tasks: ['build']
       },
       jekyll : {
@@ -64,7 +64,7 @@ module.exports = function(grunt) {
         tasks: ['jekyll:generate']
       }
     },
-    
+
     // https://github.com/dannygarcia/grunt-jekyll
     jekyll : {
       generate : {
@@ -83,7 +83,7 @@ module.exports = function(grunt) {
         }
       }
     }
-    
+
   });
 
   // Load tasks.
@@ -93,14 +93,14 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-jekyll');
-  
+
   // Register tasks.
   grunt.registerTask('build', ['compass:dev', 'jshint:dev', 'uglify', 'jekyll:generate']);
 
   grunt.registerTask('default', ['build', 'watch']);
-  
+
   grunt.registerTask('prod', ['clean', 'compass:prod', 'jshint:prod', 'uglify']);
-  
+
   grunt.registerTask('jk', ['jekyll:server']);
 
 };
