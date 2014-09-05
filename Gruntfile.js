6351dd448d632b417dbc8ffb3fcd53731ebc50b6
Derek Lieu <derek@developmentseed.org>
2014-09-04 17:04:29 -0700

    Gruntfile creates map-dependencies.min

--- Gruntfile.js
+++ Gruntfile.js
@@ -58,8 +58,7 @@ module.exports = function(grunt) {
           './assets/scripts/vendor/jquery-1.11.0.min.js': ['source_assets/scripts/vendor/jquery-1.11.0.min.js'],
           './assets/scripts/vendor/jquery-2.1.0.min.js': ['source_assets/scripts/vendor/jquery-2.1.0.min.js'],
           './assets/scripts/vendor/selectivizr-1.0.2.min.js': ['source_assets/scripts/vendor/selectivizr-1.0.2.min.js'],
-          './assets/scripts/vendor/d3.v3.min.js': ['source_assets/scripts/vendor/d3.v3.min.js'],
-          '.assets/scripts/vendor/map-dependencies.min.js' : ['source_assets/scripts/vendor/map-dependencies/*.js'],
+          './assets/scripts/vendor/map-dependencies.min.js' : ['source_assets/scripts/vendor/d3.v3.min.js', 'source_assets/scripts/vendor/map-dependencies/*.js'],
         }
       }
     },
