b89e9f2cb37c2842dbc4c80245a0383b59d4a39a
Daniel Silva <daniel.silva@flipside.org>
2014-09-11 12:34:06 +0100

    Remove obsolete code

--- source_assets/scripts/main.js
+++ source_assets/scripts/main.js
@@ -1,21 +1,5 @@
 $(function() {
-  /*
-  $('#section-switch a').click(function(e) {
-    e.preventDefault();
-    
-    // Remove all actives.
-    $('#section-switch li').removeClass('active');
-    // Activate current.
-    $(this).parent('li').addClass('active');
-    
-    // Hide all content.
-    $('.tab-content').addClass('hidden');
-    //Show clicked
-    var dest = $(this).attr('href');
-    $(dest).removeClass('hidden');
-  });
-  */
-  
+
   $('[data-toggle="dropdown"]').click(function(e) {
     e.preventDefault();
     e.stopPropagation();
