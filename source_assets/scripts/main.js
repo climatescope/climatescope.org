85a7b00d2e2f7eb37cf133b073ce7314ffad9b49
Daniel Silva <daniel.silva@flipside.org>
2014-09-11 11:46:45 +0100

    Add cookie to control modal

--- source_assets/scripts/main.js
+++ source_assets/scripts/main.js
@@ -30,4 +30,16 @@ $(function() {
     $('.dropdown.open').removeClass('open');
   });
   
+  // Modal cookie.
+  // Only show modal the first time the user enters the page.
+  // It will be language dependent.
+  if (readCookie('welcome_modal') != 'hide') {
+    $('#welcome').addClass('revealed');
+  }
+  
+  $('[data-modal-dismiss]').click(function(e) {
+    e.preventDefault();
+    createCookie('welcome_modal', 'hide', 10, '/' + CS.lang);
+  });
+  
 });
