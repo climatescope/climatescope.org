8c9717120df3d7b2a3a3c57ccd22da420d7f8d70
Daniel Silva <daniel.silva@flipside.org>
2014-10-29 14:16:26 +0000

    Add local copy of angular fixing blocked requests.

--- Gruntfile.js
+++ Gruntfile.js
@@ -66,6 +66,7 @@ module.exports = function(grunt) {
           './assets/scripts/vendor/map-dependencies.min.js' : ['source_assets/scripts/vendor/map-dependencies/*.js', 'source_assets/vendor/mapbox/mapbox.js'],
           './assets/scripts/vendor/jquery-1.11.0.min.js': ['source_assets/scripts/vendor/jquery-1.11.0.min.js'],
           './assets/scripts/vendor/jquery-2.1.0.min.js': ['source_assets/scripts/vendor/jquery-2.1.0.min.js'],
+          './assets/scripts/vendor/angular-deps.min.js': ['source_assets/scripts/vendor/angular.min.js', 'source_assets/scripts/vendor/angular-route.min.js'],
         }
       }
     },
@@ -90,6 +91,7 @@ module.exports = function(grunt) {
           './assets/scripts/vendor/map-dependencies.min.js' : ['source_assets/scripts/vendor/map-dependencies/*.js', 'source_assets/vendor/mapbox/mapbox.js'],
           './assets/scripts/vendor/jquery-1.11.0.min.js': ['source_assets/scripts/vendor/jquery-1.11.0.min.js'],
           './assets/scripts/vendor/jquery-2.1.0.min.js': ['source_assets/scripts/vendor/jquery-2.1.0.min.js'],
+          './assets/scripts/vendor/angular-deps.min.js': ['source_assets/scripts/vendor/angular.min.js', 'source_assets/scripts/vendor/angular-route.min.js'],
         }
       },
     },
--- source_assets/scripts/charts.js
+++ source_assets/scripts/charts.js
@@ -1,6 +1,5 @@
 /* jshint unused: false */
 function chart__installed_capacity(element_id) {
-  var _self = this;
   // Svg element containing the chart.
   var chart_container = d3.select("#" + element_id);
   var $chart_container = $("#" + element_id);
@@ -230,11 +229,11 @@ function chart__installed_capacity(element_id) {
       .style('display', 'none')
       .attr('class', 'tooltip-map left tooltip-chart');
     
-    _self.draw_chart();
+    draw_chart();
   });
  
   
-  _self.draw_chart = function() {
+  var draw_chart = function() {
     var w = $chart_container.width();
     var h = $chart_container.height();
 
@@ -313,341 +312,26 @@ function chart__installed_capacity(element_id) {
     /////////////////////////////////////////////////////
     // Append Axis.
   
-    chart_container.select(".x.axis")
+    d3.select(".x.axis")
       .attr("transform", "translate(0," + (height + 32) + ")")
       .call(xAxis);
       
-    chart_container.select(".x.axis .label")
+    d3.select(".x.axis .label")
       .attr("x", width)
       .attr("y", -12)
       .text(meta_info['label-x']);
   
-    chart_container.select(".y.axis")
+    d3.select(".y.axis")
       .attr("transform", "translate(-32,0)")
       .call(yAxis);
    
-    chart_container.select(".y.axis .label")
+    d3.select(".y.axis .label")
       .attr("y", 20)
       .text(meta_info['label-y']);
 
   };
   
   return {
-    draw: _self.draw_chart
+    draw: draw_chart
   };
 }
-
-
-
-
-
-
-
-
-
-/* jshint unused: false */
-function chart__clean_energy_investments(element_id) {
-  var _self = this;
-  // Svg element containing the chart.
-  var chart_container = d3.select("#" + element_id);
-  var $chart_container = $("#" + element_id);
-  var svg = chart_container.append("svg")
-    .append("g")
-      .attr('class', 'chart-container');
-      
-  /////////////////////////////////////////////////////
-  // Svg element that will contain others.
-
-  // Group to hold the area delimiter lines.
-  var line_group = svg.append("g")
-    .attr("class", "line-group");
-
-  // Group to hold the area delimiter line points.
-  var points_group = svg.append("g")
-    .attr("class", "line-points-group");
-    
-  /////////////////////////////////////////////////////
-  // Focus elements.
-  // Focus line and circles show on hover.
-
-  // Group to hold the focus elements.
-  var focus = svg.append('g')
-    .attr('class', 'focus-elements')
-    .style('display', 'none');
-  
-  // Vertical focus line.
-  focus.append('line')
-   .attr('class', 'focus-line');
-      
-  /////////////////////////////////////////////////////
-  // Append Axis.
-
-  svg.append("g")
-    .attr("class", "x axis")
-    .append("text")
-      .attr("class", "label")
-      .style("text-anchor", "end");
-
-  svg.append("g")
-    .attr("class", "y axis")
-    .append("text")
-      .attr("class", "label")
-      .attr("transform", "rotate(-90)")
-      .style("text-anchor", "end");
-  
-  /////////////////////////////////////////////////////
-  // Draw functions.
-  
-  // X scale. Range updated in function.
-  var x = d3.scale.linear();
-
-  // Y scale. Range updated in function.
-  var y = d3.scale.linear();
-
-  // Define xAxis function.
-  var xAxis = d3.svg.axis()
-    .scale(x)
-    .tickFormat(function(d) {
-      return d.toString().substr(2);
-    })
-    .orient("bottom");
-
-  // Define yAxis function.
-  var yAxis = d3.svg.axis()
-    .scale(y)
-    .ticks(6)
-    .orient("left");
-
-  // Line function for the delimit the area.
-  var line = d3.svg.line()
-    .x(function(d) { return x(d.year); })
-    .y(function(d) { return y(d.value); });
-  
-  /////////////////////////////////////////////////////
-  // Data request.
-
-  var chart_data;
-  var meta_info;
-  var margin = {top: 20, right: 20, bottom: 82, left: 100};
-  var width, height;
-
-  // Request data.
-  //var id = CS.stateId ? CS.stateId : CS.countryId;
-  //var url = CS.domain + '/' + CS.lang + '/api/auxiliary/installed-capacity/' + id + '.json';
-  var url = '/to_delete.json';
-  d3.json(url, function(error, DATA) {
-    if (error) {
-      return console.log(error);
-    }
-    
-    meta_info = DATA.meta;
-    chart_data = DATA.data;
-    
-    // Domain for the X Axis.
-    // Since the years are the same for all the data, just create
-    // the domain from one.
-    x.domain(d3.extent(chart_data[0].values, function(d) { return d.year; }));
-    
-    // Compute the y domain taking into account the stacked values.
-    y.domain([0, d3.max(chart_data, function (c) { 
-      return d3.max(c.values, function (d) { return d.value; });
-    })]);
-
-    // Groups to hold the focus circles.
-    // One group per line. Will hold two circles:
-    // An outer and bigger and an inner and smaller one
-    var focus_circles = focus.selectAll("g")
-      .data(chart_data)
-        .enter().append('g')
-        .attr('class', function(d) {
-          return 'focus-circles ' + d.id;
-        });
-    
-    // Outer circle.
-    focus_circles.append('circle')
-      .attr("r", 8)
-      .attr('class', 'outer');
-  
-    // Inner circle.
-    focus_circles.append('circle')
-      .attr("r", 3)
-      .attr('class', 'inner');
-    
-    // Add focus rectangle. Will be responsible to trigger the events.
-    svg.append("rect")
-      .attr('class', 'trigger-rect')
-      .style("fill", "none")
-      .style("pointer-events", "all")
-      .on("mouseover", function() { focus.style("display", null); chart_tooltip.style("display", null); chart_container.classed({mouseover: true}); })
-      .on("mouseout", function() { focus.style("display", "none"); chart_tooltip.style("display", "none"); chart_container.classed({mouseover: false}); })
-      .on("mousemove", function() {
-        // Define bisector function. Is used to find the closest year
-        // to the mouse position.
-        var bisector = d3.bisector(function(d) { return d.year; }).left;
-        var mousex = x.invert(d3.mouse(this)[0]);
-    
-        var xpos;
-        var doc_index;
-        // Position the circles.
-        focus.selectAll(".focus-circles circle") 
-          .attr("transform", function(d) {
-            var closest_year = Math.round(mousex);
-            doc_index = bisector(d.values, closest_year);
-            var doc = d.values[doc_index];
-            
-            xpos = x(doc.year);
-    
-            return "translate(" + x(doc.year) + "," +  y(doc.value) + ")";
-          });
-    
-        // Position the focus line.
-        focus.select('.focus-line')
-          .attr('x1', xpos).attr('y1', height)
-          .attr('x2', xpos).attr('y2', 0);
-
-        // Position tooltip.
-        chart_tooltip
-          .attr('style', function() {
-            // Calculate tooltip width, taking the margin into account.
-            // add some extra margin.
-            var tooltip_width = chart_tooltip.style('width').replace('px', '');
-            // Remove left and right classes.
-            chart_tooltip.classed({right: false, left: false});
-            
-            // Get chart container position in space.
-            // Use jQuery object since it's easier.
-            var container_left = $chart_container.offset().left;
-            // By default the tooltip will be left of the line.
-            // Calc where the tooltip would be if aligned to the left.
-            var new_xpos = xpos - tooltip_width;
-            // If it doesn't fit in the viewport show it at the right.
-            if (container_left + new_xpos <= 0) {
-              chart_tooltip.classed({right: true});
-              return 'transform: translate(' + (xpos + margin.left + 10) + 'px, 50px)';
-            }
-            else {
-              chart_tooltip.classed({left: true});
-              return 'transform: translate(' + (xpos - tooltip_width + margin.left - 10) + 'px, 50px)';
-            }
-            
-          })
-          .html(function() {
-            var content = '<div class="tooltip-inner">';
-            content += '<dl class="chart-legend">';
-
-            // Reverse the array to ensure the order is the same.
-            chart_data.slice(0).reverse().forEach(function(doc) {
-              // Correct object from values array.
-              var value_doc = doc.values[doc_index];
-              content += '<dt class="param-' + doc.id + '">' + doc.name + '</dt>';
-              content += '<dd>' + value_doc.value + '</dd>';
-            });
-
-            content += '</dl>';
-            content += '</div>';
-
-            return content;
-          });
-    });
-    
-    var chart_tooltip = chart_container.append('div')
-      .style('display', 'none')
-      .attr('class', 'tooltip-map left tooltip-chart');
-
-    _self.draw_chart();
-  });
- 
-  
-  _self.draw_chart = function() {
-    var w = $chart_container.width();
-    var h = $chart_container.height();
-
-    // Size..
-    width = w - margin.left - margin.right;
-    height = h - margin.top - margin.bottom;
-      
-    // Set chart size.
-    chart_container.select("svg")
-      //.attr("width", width + margin.left + margin.right)
-      //.attr("height", height + margin.top + margin.bottom);
-      .attr("width", "100%")
-      .attr("height", "100%");
-
-    // Chart container translate.
-    chart_container.select(".chart-container")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-    
-    chart_container.select(".trigger-rect")
-      .attr("width", width)
-      .attr("height", height);
-    
-    // Update scale ranges
-    x.range([0, width]);
-    y.range([height, 0]);
-
-    // Area delimiters
-    var lines = line_group.selectAll("path")
-      .data(chart_data);
-    // Handle new.
-    lines.enter().append("path");
-    // Remove old.
-    lines.exit().remove();
-    // Update current.
-    lines
-        .attr("d", function(d) { return line(d.values); })
-        .attr("class", function(d) { return "line " + d.id; });
-
-    // Group to hold the line points.
-    // One group for each line points.
-    var points = points_group.selectAll(".line-points")
-      .data(chart_data);
-    // Handle new.
-    points.enter().append("g");
-    // Remove old.
-    points.exit().remove();
-    // Update current.
-    points
-        .attr("class", function(d) { return "line-points " + d.id; });
-
-    // Add the points.
-    var individual_points = points.selectAll("circle")
-      .data(function(d) { return d.values; });
-    // Handle new.
-    individual_points.enter().append('circle');
-    // Remove old.
-    individual_points.exit().remove();
-    // Update current.
-    individual_points
-      .attr("cx", function(d) { return x(d.year); })
-      .attr("cy", function(d) { return y(d.value); })
-      .attr("r", 3);
-
-    /////////////////////////////////////////////////////
-    // Append Axis.
-  
-    chart_container.select(".x.axis")
-      .attr("transform", "translate(0," + (height + 32) + ")")
-      .call(xAxis);
-      
-    chart_container.select(".x.axis .label")
-      .attr("x", width)
-      .attr("y", -12)
-      .text(meta_info['label-x']);
-  
-    chart_container.select(".y.axis")
-      .attr("transform", "translate(-32,0)")
-      .call(yAxis);
-   
-    chart_container.select(".y.axis .label")
-      .attr("y", 20)
-      .text(meta_info['label-y']);
-
-  };
-  
-  return {
-    draw: _self.draw_chart
-  };
-}
-setTimeout(function() {
-chart__clean_energy_investments('chartcc');
-},10);
