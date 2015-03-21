cd1ee60c171b80337baecca3fd59cbd69f98db04
Daniel Silva <daniel.silva@flipside.org>
2015-03-21 12:57:19 +0000

    Add states to compare and style page

--- source_assets/scripts/angular/controllers/compare_controller.js
+++ source_assets/scripts/angular/controllers/compare_controller.js
@@ -1,5 +1,5 @@
 (function() {
-  var app = angular.module('compareApp', ['ngRoute', 'ui.bootstrap'], function($interpolateProvider) {
+  var app = angular.module('compareApp', ['ngRoute', 'ui.bootstrap', 'mathFilters'], function($interpolateProvider) {
     $interpolateProvider.startSymbol('%%');
     $interpolateProvider.endSymbol('%%');
   });
@@ -8,14 +8,18 @@
   function($http, $filter, $rootScope, $scope, $location) {
     var _self = this;
     this.countries = [];
+    // Init options to null to prevent the select from adding an empty option.
     this.compare = [];
+    // Allows us to show the countries only when the button is pressed.
+    this.compareSelected = [];
 
     this.apply = function() {
       console.log(_self.compare);
       var qstring = {
-        compare: _self.compare[0] ? _self.compare[0].iso : null,
-        with: _self.compare[1] ? _self.compare[1].iso : null
+        compare: _self.compare[0] ? _self.compare[0] : null,
+        with: _self.compare[1] ? _self.compare[1] : null
       };
+      computeSelected();
       $location.search(qstring);
     };
 
@@ -27,6 +31,24 @@
       $location.search(qstring);
 
       _self.compare = [];
+      _self.compareSelected = [];
+    };
+
+    this.getOptgroupLabel = function(country) {
+      switch (country.iso) {
+        case 'cn':
+          return CS.t('Chinese provinces');
+        case 'in':
+          return CS.t('Indian states');
+      }
+    };
+
+    this.getRegionUrl = function(id) {
+      return CS.regionIndex[id];
+    };
+
+    this.getCountryUrl = function(iso) {
+      return CS.countryIndex[iso];
     };
 
     var getCountry = function(prop, val) {
@@ -38,27 +60,66 @@
       return null;
     };
 
-    var processQuerystring = function() {
-      var qstring = $location.search();
-      _self.compare = [];
-      // Use "compare" and "with" to make the url prettier.
-      if (qstring.compare) {
-        _self.compare[0] = getCountry('iso', qstring.compare.toLowerCase());
+    var getState = function(iso) {
+      var countryIso = iso.split('-');
+      var country = getCountry('iso', countryIso[0]);
+
+      if (!country) {
+        return null;
       }
-      if (qstring.with) {
-        _self.compare[1] = getCountry('iso', qstring.with.toLowerCase());
+
+      for (var i in country.states) {
+        if (country.states[i].iso == iso) {
+          return country.states[i];
+        }
       }
+      return null;
+    };
+
+    var computeSelected = function() {
+      for (var c in _self.compare) {
+        if (_self.compare[c] && _self.compare[c].length > 3) {
+          var countryIso = _self.compare[c].split('-');
+          var country = getCountry('iso', countryIso[0]);
+          // State
+          _self.compareSelected[c] = getState(_self.compare[c]);
+          // Store a reference to the parent to be able to show the subtitle.
+          if (_self.compareSelected[c]) {
+            _self.compareSelected[c].parent = country;
+          }
+        }
+        else {
+          // Country
+          _self.compareSelected[c] = getCountry('iso', _self.compare[c]);
+        }
+      }
+
+      console.log('_self.compareSelected', _self.compareSelected);
+      console.log('_self.compare', _self.compare);
     };
 
     $http.get(CS.domain + '/' + CS.lang + '/api/countries.json').success(function(data) {
       _self.countries = data;
-      processQuerystring();
-      console.log('request');
+      computeSelected();
+      console.log('request finished');
     });
 
     $rootScope.$on('$locationChangeSuccess', function() {
       console.log('$locationChangeSuccess');
-      processQuerystring();
+      
+      var qstring = $location.search();
+
+      if (qstring.compare) {
+        _self.compare[0] = qstring.compare.toLowerCase();
+      }
+      if (qstring.with) {
+        _self.compare[1] = qstring.with.toLowerCase();
+      }
+
+      // Update url of share options.
+      $scope.currentPath = $location.url();
+
+      computeSelected();
     });
 
   }]);
