c94ef9f86b93bb5ba4bf5df104e292d51f369b0b
smit1678 <nate@developmentseed.org>
2015-11-15 20:31:44 -0500

    switching to a new publish branch for deployment

--- .build_scripts/deploy.sh
+++ .build_scripts/deploy.sh
@@ -2,7 +2,7 @@
 set -e # halt script on error
 
 # If this is the publish branch, push it up to gh-pages
-if [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH = "master" ]; then
+if [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH = "publish" ]; then
   echo "Get ready, we're publishing!"
   cd _site
   git init
@@ -11,7 +11,7 @@ if [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH = "master" ]; then
   rm ./CNAME
   git add .
   git commit -m "CI deploy to gh-pages"
-  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
+  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:master
 else
   echo "Not a publishable branch so we're all done here"
 fi
