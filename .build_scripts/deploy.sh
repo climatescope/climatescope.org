30552d1fa25c7b249946ad655c7f058960f0a523
Olaf Veerman <olaf.veerman@gmail.com>
2016-11-15 22:41:01 -0500

    Update travis build with latest and greatest

--- .build_scripts/deploy.sh
+++ .build_scripts/deploy.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
-# If this is the publish branch, push it up to gh-pages
-if [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH = "publish" ]; then
-  echo "Get ready, we're publishing!"
-  cd _site
-  git init
-  git config user.name "DevSeed Build Bot"
-  git config user.email "dsbb@developmentseed.org"
-  git add .
-  git commit -m "CI deploy to gh-pages"
-  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:master
-else
-  echo "Not a publishable branch so we're all done here"
-fi
+echo "Get ready, we're pushing to gh-pages!"
+cd _site
+cp ../CNAME ./
+git init
+git config user.name "DevSeed Build Bot"
+git config user.email "dsbb@developmentseed.org"
+git add .
+git commit -m "CI deploy to gh-pages"
+git push --force --quiet "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" master:gh-pages
