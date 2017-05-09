180d50a39e63b97e4549801393b2a8365cbef4ac
Olaf Veerman <olaf.veerman@gmail.com>
2017-05-08 23:00:41 -0400

    Start using a deploy key

--- .build_scripts/deploy.sh
+++ .build_scripts/deploy.sh
@@ -1,11 +1,29 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
-echo "Get ready, we're pushing to gh-pages!"
+echo "Deploying to gh-pages!"
 cd _site
+
+# Save some useful information
+SSH_REPO="git@github.com:${TRAVIS_REPO_SLUG}.git"
+SHA=`git rev-parse --verify HEAD`
+
+# Initialize repo
 git init
 git config user.name "DevSeed Build Bot"
 git config user.email "dsbb@developmentseed.org"
 git add .
-git commit -m "CI deploy to gh-pages"
-git push --force --quiet "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" master:gh-pages
+git commit -m "CI deploy to master ${SHA}"
+git show-ref
+
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../.build_scripts/deploy_key.enc -out deploy_key -d
+chmod 600 deploy_key
+eval `ssh-agent -s`
+ssh-add deploy_key
+
+git push --force --quiet $SSH_REPO master:gh-pages
