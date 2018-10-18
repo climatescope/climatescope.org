9c1cb187e6686eb896bb4ae6868a1be5aa882a3a
Daniel da Silva <daniel@developmentseed.org>
2018-10-18 17:36:59 +0100

    Add content library page and restructure navigation

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -11,6 +11,7 @@ import history from './utils/history'
 import Home from './views/home'
 import StaticPage from './views/static-page'
 import Download from './views/download'
+import Library from './views/library'
 import Policies from './views/policies-hub'
 import Policy from './views/policies-page'
 import Results from './views/results'
@@ -25,6 +26,7 @@ const Root = () => (
         <Route exact path='/' component={Home}/>
         <Route exact path='/results' component={Results}/>
         <Route exact path='/download' component={Download}/>
+        <Route exact path='/library' component={Library}/>
         <Route path='/compare/:country*' component={Compare}/>
         <Route exact path='/policies/:policyId' component={Policy}/>
         <Route exact path='/policies' component={Policies}/>
---
+++ app/assets/scripts/views/library.js
@@ -0,0 +1,68 @@
+'use strict'
+import React from 'react'
+import { PropTypes as T } from 'prop-types'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+
+import { environment } from '../config'
+
+import App from './app'
+import ShareOptions from '../components/share'
+
+class Library extends React.Component {
+  render () {
+    return (
+      <App>
+        <article className='layout--page full'>
+          <header className='layout--page__header'>
+            <div className='row--contained'>
+              <div className='layout--page__heading'>
+                <h1 className='layout--page__title'>Content library</h1>
+              </div>
+              <div className='layout--page__tools'>
+                <ul className='actions-menu'>
+                  <li><ShareOptions url={window.location.toString()} /></li>
+                </ul>
+              </div>
+            </div>
+          </header>
+
+          <div className='layout--page__body'>
+            <div className='row--contained'>
+              <div className='col--main prose-copy'>
+                Content library includes Insights and Tools, displayed as visually engaging tiles.
+                <h2>Tools</h2>
+                <ul className="well-list">
+                  <li><Link to='/compare' title='View results side by side'>Geography Comparison</Link></li>
+                  <li><Link to='/off-grid-data-hub' title='Use the Off-grid Data Hub'>Off-grid Data Hub</Link></li>
+                  <li><Link to='/clean-energy-investments' title='Use the Clean Energy Investment'>Clean Energy Investment</Link></li>
+                  <li><Link to='/capacity-generation' title='Use the Capacity Generation'>Capacity Generation</Link></li>
+                  <li><Link to='/policies' title='Browse the policy database'>Policies</Link></li>
+                  <li><Link to='/download' title='Download the data and report'>Download</Link></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+        </article>
+      </App>
+    )
+  }
+}
+
+if (environment !== 'production') {
+  Library.propTypes = {
+  }
+}
+
+function mapStateToProps (state, props) {
+  return {
+  }
+}
+
+function dispatcher (dispatch) {
+  return {
+  }
+}
+
+export default connect(mapStateToProps, dispatcher)(Library)
