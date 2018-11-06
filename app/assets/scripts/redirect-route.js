77e9f7b1e3312defd08970c58f12b5ab8275f883
Daniel da Silva <daniel@developmentseed.org>
2018-11-06 10:42:55 +0000

    Add redirects for insights and off grid
    Contribute to #304

--- app/assets/scripts/redirect-route.js
+++ app/assets/scripts/redirect-route.js
@@ -94,7 +94,26 @@ const redirectPaths = {
   '/en/country/venezuela': '/results/ve',
   '/en/country/vietnam': '/results/vn',
   '/en/country/zambia': '/results/zm',
-  '/en/country/zimbabwe': '/results/zw'
+  '/en/country/zimbabwe': '/results/zw',
+
+  // Blog:
+  '/en/blog': 'https://medium.com/climatescope',
+
+  // Insights:
+  '/en/insights/risk-management': 'https://medium.com/climatescope/how-to-mitigate-renewables-risks-in-emerging-markets-514b6afade97',
+  '/en/insights/frontier-power': 'https://medium.com/climatescope/distributed-energy-in-emerging-markets-a6319604e947',
+  '/en/insights/energy-policy': 'https://medium.com/climatescope/policies-for-the-energy-transition-lessons-learned-in-emerging-markets-ff9aa3d78b78',
+  '/en/insights/emerging-markets-investment': 'https://medium.com/climatescope/emerging-markets-clean-energy-investment-4b7b37f8e3de',
+  '/en/insights/climate-policy': 'https://medium.com/climatescope/clean-energy-and-the-paris-promises-e5116c1c7000',
+
+  // Off grid quarterly:
+  '/en/off-grid-quarterly/q3-2018': 'https://medium.com/climatescope/3q-2018-off-grid-and-mini-grid-market-outlook-e4ae33657c9c',
+  '/en/off-grid-quarterly/q2-2018': 'https://medium.com/climatescope/2q-2018-off-grid-and-mini-grid-market-outlook-2504a1d4abb0',
+  '/en/off-grid-quarterly/1q-2018': 'https://medium.com/climatescope/1q-2018-off-grid-and-mini-grid-market-outlook-d097168dc92e',
+  '/en/off-grid-quarterly/q4-2017': 'https://medium.com/climatescope/4q-2017-off-grid-and-mini-grid-market-outlook-e3f5552740c2',
+  '/en/off-grid-quarterly/q3-2017': 'https://medium.com/climatescope/3q-2017-off-grid-and-mini-grid-market-outlook-5ba98bb5bc6b',
+  '/en/off-grid-quarterly/q2-2017': 'https://medium.com/climatescope/2q-2017-off-grid-and-mini-grid-market-outlook-612fc3b7779c',
+  '/en/off-grid-quarterly/q1-2017': 'https://medium.com/climatescope/1q-2017-off-grid-and-mini-grid-market-outlook-bab39128a197'
 }
 
 // Redirect handling.
