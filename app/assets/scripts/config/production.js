c6e5f5ba2440cd8be1d75f1c0f72a17e51da56ca
Daniel da Silva <daniel@developmentseed.org>
2018-11-27 18:03:53 +0000

    Fix Jekyll metatags

--- app/assets/scripts/config/production.js
+++ app/assets/scripts/config/production.js
@@ -4,9 +4,6 @@
 export default {
   ...CS.config,
   environment: 'production',
-  appTitle: 'Climatescope',
-  appEdition: 2018,
-  appDescription: 'The Climatescope a unique country-by-country assessment, interactive report and index that evaluates the investment climate for climate-related investment worldwide.',
   policyDbUrl: 'https://services.bnef.com/public-api',
   mediumLatestUrl: 'https://cs-medium-latest.herokuapp.com',
   mailchimpUrl: '',
