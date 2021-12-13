7027ee74ff832890ffac80f47d21753765d9dd5d
zimrick <richard@zcreativelabs.com>
2021-12-13 16:16:22 +0100

    Removed deprecated app and build files

--- app/assets/scripts/config/staging.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-
-export default {
-  environment: 'staging',
-  gaTrackingID: null
-}
--- app/assets/scripts/utils/history.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-// We will need to access history from outside components.
-// The only way to do this is create our own history and pass it to the router.
-// https://github.com/ReactTraining/react-router/blob/master/FAQ.md#how-do-i-access-the-history-object-outside-of-components
-import { createBrowserHistory } from 'history'
-
-export default createBrowserHistory()
