5f0dd7effa19e1cc6d315f2fe10a45d1d1824be8
zimrick <richard@zcreativelabs.com>
2023-11-26 13:59:09 +0100

    Removed out folder

--- out/_next/static/ThHgspejfCbg5tLLIEPVh/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fthemes","\u002Fsectors","\u002Ftools","\u002Fblog","\u002Fmarkets","\u002Fresults","\u002Fhighlights","\u002F","\u002Fabout","\u002Fmarkets\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
