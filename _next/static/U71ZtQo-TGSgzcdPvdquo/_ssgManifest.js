746c6b6f6afdbdb93792c8f3a7f305a4aa191ddb
zimrick <zimrick@users.noreply.github.com>
2023-11-30 23:08:45 +0000

    deploy: 5d56b8538365a27280d2243fa31a162adf957051

---
+++ _next/static/U71ZtQo-TGSgzcdPvdquo/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fabout","\u002Fabout\u002F[slug]","\u002Fblog","\u002Fblog\u002F[slug]","\u002Fhighlights","\u002Fmarkets","\u002Fmarkets\u002F[slug]","\u002Fresults","\u002Fsectors","\u002Fsectors\u002F[slug]","\u002Ftools","\u002Ftools\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
