afe293e21fd6f608e4f4f521897d6280fda79c34
zimrick <richard@zcreativelabs.com>
2023-11-30 20:25:16 +0100

    Added irst draft of area bubbles

--- pages/areas.js
+++ pages/areas.js
@@ -1,65 +1,175 @@
-import { Box, Center, Container } from "@chakra-ui/layout"
+import { useEffect, useState } from "react"
+import { Box, Container, SimpleGrid } from "@chakra-ui/layout"
 import getConfig from "next/config"
+import { scaleSqrt, scaleThreshold } from "d3-scale"
+// import * as projies from "d3-geo-projection"
+// import { geoMercator, geoEqualEarthRaw } from "d3-geo"
+import * as d3Geo from "d3-geo"
+const { geoMercator, geoEqualEarth } = d3Geo
+import _sortBy from "lodash/sortBy"
+import { forceSimulation, forceX, forceY, forceCollide } from "d3-force"
 import {
   ComposableMap,
-  Marker,
   Geographies,
   Geography,
+  Sphere,
+  Graticule,
 } from "react-simple-maps"
-import { scaleSqrt } from "d3-scale"
-import { geoMollweide } from "d3-geo-projection"
 
 import { useClientData } from "@utils/api/client"
 
 const { publicRuntimeConfig } = getConfig()
 const basePath = publicRuntimeConfig.basePath
 
+const geoUrl = `${basePath}/data/world-sans-antarctica.json`
 const dataUrl = `${basePath}/data/areas-by-geography.json`
 
 export default function AreasByGeography() {
   const { data, error } = useClientData(dataUrl)
-  if (error) return <div>{"No data"}</div>
+  const [points, setPoints] = useState([])
 
-  const sizeScale = scaleSqrt().domain([1, 175000000]).range([2, 120])
+  const sizeDomain = [1, 175000000]
+  const sizeRange = [3, 180]
+  const sizeScale = scaleSqrt().domain(sizeDomain).range(sizeRange)
 
   const width = 800
   const height = 500
 
-  // const proj = geoMollweide()
-  //   .rotate([...coordinates, 0])
-  //   .translate([])
-  //   .fitExtent(
-  //     [
-  //       [padding, padding],
-  //       [400 - padding, 400 - padding],
-  //     ],
-  //     selectedGeo
-  //   )
+  const features =
+    _sortBy(
+      data?.map(({ lon, lat, ...properties }) => {
+        return {
+          type: "Feature",
+          geometry: { type: "Point", coordinates: [lon, lat] },
+          properties,
+        }
+      }),
+      (o) => -o.properties.area
+    ) || []
+
+  const padding = sizeRange[1] / 4
+
+
+  const proj = geoEqualEarth()
+    // .fitSize([width - padding * 2, height - padding * 2], { type: "Sphere" })
+    // .translate([width / 2, height / 2])
+    // .rotate([-10, 0, 0])
+    .fitExtent(
+      [
+        [padding, padding],
+        [width - padding, height - padding],
+      ],
+      {
+        type: "FeatureCollection",
+        features: [
+          // {
+          //   type: "Feature",
+          //   geometry: { type: "Point", coordinates: [180, 0] },
+          // },
+          // {
+          //   type: "Feature",
+          //   geometry: { type: "Point", coordinates: [-180, 0] },
+          // },
+          ...features,
+        ],
+      }
+    )
+    .translate([width / 2, height / 2])
+    .rotate([-25, 0, 0])
+    .center([0, 15])
+
+  useEffect(() => {
+    if (!features?.length) return undefined
+    if (points?.length) return undefined
+
+    // 1. Configure simulation
+    const simulation = forceSimulation([...features])
+      .force(
+        "x",
+        forceX((d) => Math.round(proj(d.geometry.coordinates)[0]))
+      )
+      .force(
+        "y",
+        forceY((d) => Math.round(proj(d.geometry.coordinates)[1]))
+      )
+      .force(
+        "collide",
+        forceCollide((d) => Math.round(sizeScale(d.properties.area)) + 0.5)
+      )
+      .stop()
+
+    // 2. Run simulation
+    for (let i = 0; i < 200; i++) simulation.tick()
+
+    // 3. Set new points
+    setPoints(simulation.nodes())
+  }, [features])
+
+  if (error) return <div>{"No data"}</div>
+  if (!data) return <div>{"Loading data..."}</div>
+
+  const fontSizeScale = scaleThreshold()
+    .domain([sizeRange[0], sizeRange[0] * 2, sizeRange[0] * 10, sizeRange[1]])
+    .range([0, 4, 6, 9])
+  const fontWeightScale = scaleThreshold()
+    .domain([sizeRange[0], sizeRange[0] * 2, sizeRange[0] * 10, sizeRange[1]])
+    .range([0, 700, 600, 500])
 
   return (
     <Container pb={24}>
-      <Center h="100vh" bg="gray.100">
-        <Box bg="white" w="100%">
-          <ComposableMap width={800} height={500}>
-            {data?.map((geo) => {
+      <SimpleGrid bg="gray.50" w="100%" columns={1} gridGap={0}>
+        <Box gridColumn="1 / -1" gridRow="1 / span 1">
+          <ComposableMap width={width} height={height} projection={proj}>
+            <Sphere />
+            <Geographies geography={geoUrl}>
+              {({ borders, outline }) => {
+                return (
+                  <>
+                    <Geography
+                      key="outline"
+                      geography={outline}
+                      fill="none"
+                      stroke="#999"
+                      strokeWidth={0.5}
+                    />
+                    <Geography
+                      key="borders"
+                      geography={borders}
+                      fill="none"
+                      stroke="#999"
+                      strokeWidth={0.5}
+                    />
+                  </>
+                )
+              }}
+            </Geographies>
+          </ComposableMap>
+        </Box>
+        <Box gridColumn="1 / -1" gridRow="1 / span 1">
+          <svg viewBox={`0 0 ${width} ${height}`}>
+            {points.map((point) => {
+              const r = sizeScale(point.properties.area)
               return (
-                <Marker key={geo.iso} coordinates={[geo.lon, geo.lat]}>
-                  <circle r={sizeScale(geo.area)} />
+                <g
+                  key={point.properties.iso}
+                  transform={`translate(${point.x} ${point.y})`}
+                >
+                  <circle r={r} />
                   <text
                     textAnchor="middle"
                     alignmentBaseline="central"
-                    fontSize={8}
-                    fontWeight={600}
-                    fill="#FFF"
+                    fill="white"
+                    fontSize={fontSizeScale(r)}
+                    fontWeight={fontWeightScale(r)}
                   >
-                    {geo.iso.toUpperCase()}
+                    {point.properties.iso.toUpperCase()}
                   </text>
-                </Marker>
+                </g>
               )
             })}
-          </ComposableMap>
+          </svg>
         </Box>
-      </Center>
+      </SimpleGrid>
     </Container>
   )
 }
