59462e9c94770145687542900239424c251e4d01
this-is-cris <this-is-cris@users.noreply.github.com>
2024-11-12 00:24:07 +0000

    deploy: 37eedf71ee6168f6160c58426383666057c5af1d

---
+++ _next/static/keguxcx8jsUT9QFYgl43f/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fabout","\u002Fabout\u002F[slug]","\u002Fhighlights","\u002Fhighlights\u002F[slug]","\u002Fmarkets\u002F[slug]","\u002Fresults","\u002Ftools","\u002Ftools\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
